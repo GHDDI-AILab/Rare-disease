0da085954062bda1d16b1341c5ff0b1a8b234fcd
XiaoyingLv <xiaoying.lv@ghddi.org>
2020-10-28 17:53:06 +0800

    source code

--- PPI/format.sh
+++ PPI/format.sh
@@ -1,5 +1,6 @@
 ###PINA multiple seprator
 awk -F '[\t;:;(]' 'FNR>1{print $6"\t"$9}' Homo\ sapiens-PINA-20140521.tsv > PINA_HGNC_edges.txt
+awk '{if(!($2=="" || $1=="")) print $0}' *HGNC_edges.txt > PINA_HGNC_edges_no_space.txt
 
 ###STRING
 awk -F '[ ;.]' 'NR==FNR{a[$1]=$3}NR>FNR{if($2 in a && $4 in a) print a[$2]"\t"a[$4]}' STRING_ENSP_HGNC_v77.txt STRING_edges.txt > STRING_HGNC_edges.txt
